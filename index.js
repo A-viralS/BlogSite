dbcf37599127ca542345fcbe1e75bb12ed50d6ec
Aviral <soniaviral14@gmail.com>
2024-01-19 09:19:57 +0530

    basic authenti

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express= require('express')
 const path=require('path')
 const app =express();
+const mongoose =require('mongoose')
+const userRouter=require('./routes/users')
+app.use(express.urlencoded({extended:false}))
+mongoose.connect('mongodb://localhost:27017/blogsite3').then(e=>console.log('mongoDB connected'));
 PORT=8000;
 app.set('view engine','ejs');
 app.set('views',path.resolve('./views'))
@@ -8,4 +12,12 @@ app.set('views',path.resolve('./views'))
 app.get('/',(req,res)=>{
     res.render('home')
 })
+app.get('/user/signup',(req,res)=>{
+    res.render('signup')
+})
+app.get('/user/signin',(req,res)=>{
+    res.render('signin')
+})
+
+app.use('/user',userRouter)
 app.listen(PORT,()=>console.log(`server started at ${PORT}`))
---
+++ models/users.js
@@ -0,0 +1,70 @@
+const { createHmac, randomBytes } = require("crypto");
+const mongoose =require('mongoose');
+const UserSchema=new mongoose.Schema({
+    fullName: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    salt:{
+        type:String,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profileImageUrl:{
+        type:String,
+        default: '../public/defaultPic.png',
+    },
+    role:{
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER"
+    }
+
+},
+{timestamps:true}
+)
+
+UserSchema.pre("save", function (next) {
+    const user = this;
+  
+    if (!user.isModified("password")) return;
+  
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+      .update(user.password)
+      .digest("hex");
+  
+    this.salt = salt;
+    this.password = hashedPassword;
+  
+    next();
+  });
+  UserSchema.static(
+    "matchPassword",
+    async function (email, password) {
+      const user = await this.findOne({ email });
+      if (!user) throw new Error("User not found!");
+  
+      const salt = user.salt;
+      const hashedPassword = user.password;
+  
+      const userProvidedHash = createHmac("sha256", salt)
+        .update(password)
+        .digest("hex");
+  
+      if (hashedPassword !== userProvidedHash)
+        throw new Error("Incorrect Password");
+  
+     return user;
+    }
+  );
+  const User=mongoose.model("user",UserSchema);// user is the name of the collections. it would be shown as users in the terminal as mongo pluralises it 
+  module.exports=User;
